8d082caa011bb8155a0ed3ed1e1d32f4ce96617f
Aman Kumar <akgupta0524@gmail.com>
2024-11-20 00:00:01 +0530

    find vs filter

---
+++ 05-FindAndFilter/index.js
@@ -0,0 +1,12 @@
+/** Differences between Filter and Find Method.
+ *
+ * Filter :-
+ *
+ * Filter() method returns the matched values in an array from the collection.
+ *
+ *
+ * Find :-
+ * Find() method returns the first value that matches from the collection,
+ * Once it matches the value in findings,
+ * it will not check the remaining values in the array collection.
+ */
