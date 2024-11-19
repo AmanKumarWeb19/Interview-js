b344d8e3fa020a7a0f32eb6db7424da232a7435f
Aman Kumar <akgupta0524@gmail.com>
2024-11-19 23:52:35 +0530

    == and ===

---
+++ 03-FInd_==&===/index.js
@@ -0,0 +1,9 @@
+/** 1. == and === both are comparison operator.
+ *  2. The main differences between both the operators is that,
+ *     "==" is used to compare only value whereas ," === " is used to compare both value and types.
+ */
+
+var x = 7;
+var y = "7";
+console.log(x == y);
+console.log(x === y);
