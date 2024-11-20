97841e781a0d55ece8cd4d7eeb0fc840ea523658
Aman Kumar <akgupta0524@gmail.com>
2024-11-20 10:30:38 +0530

    find and filter

--- 05-FindAndFilter/index.js
+++ 05-FindAndFilter/index.js
@@ -10,3 +10,20 @@
  * Once it matches the value in findings,
  * it will not check the remaining values in the array collection.
  */
+
+// Filter:-
+const empArr = [
+  { name: "Aman", age: 28 },
+  { name: "Akash", age: 20 },
+  { name: "Raj", age: 45 },
+  { name: "Adi", age: 26 },
+  { name: "Rohit", age: 40 },
+  { name: "Rahul", age: 30 },
+];
+
+const filtered = empArr.filter((item) => item.age > 30 || item.age >= 30);
+console.log(filtered);
+
+// Find:-
+const fined = empArr.find((item) => item.age > 25);
+console.log(fined);
