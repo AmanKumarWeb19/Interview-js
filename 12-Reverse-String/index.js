4f43088e4ba905cffb24843dbb61ee40cc37a872
Aman Kumar <akgupta0524@gmail.com>
2024-11-21 10:10:24 +0530

    new reverse method

--- 12-Reverse-String/index.js
+++ 12-Reverse-String/index.js
@@ -20,3 +20,21 @@ function reverseStr(str) {
 }
 let reverse = reverseStr("Aman");
 console.log(reverse);
+
+//...............................................................................................
+
+function reverseWord(str) {
+  let words = str.split(); // Split the string into words array
+  let result = []; //Initialize an array to store reversed word
+
+  for (let word of words) {
+    let reversed = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversed = reversed + word[i]; // Reverse the current word
+    }
+    result.push(reversed); // Add the reversed word to the result array
+  }
+  return result.join(""); // Join the reversed words into a single string
+}
+let result = reverseWord("Aman Kumar");
+console.log(result);
