ace7daadbe7d4f5cf7e202d374d08af7927c222b
Aman Kumar <akgupta0524@gmail.com>
2024-11-24 10:39:35 +0530

    output based

--- 29-Output-Based/index.js
+++ 29-Output-Based/index.js
@@ -1,13 +1,40 @@
 /** OUTPUT BASED:- */
 
-console.log(1);
+// Question :1 (Which result print first in the console.log())
+// console.log(1);
 
-setTimeout(() => {
-  console.log(2);
-}, 1000);
+// setTimeout(() => {
+//   console.log(2);
+// }, 1000);
 
-setTimeout(() => {
-  console.log(3);
-}, 0);
+// setTimeout(() => {
+//   console.log(3);
+// }, 0);
 
-console.log(4);
+// console.log(4);
+
+// Question:2 What is the output of this setTimeout function:
+// setTimeout(() => {
+//   console.log(i);
+//   var i = 10;
+// }, 1000);
+
+// Question 3:-
+// What is the output of this function:-
+
+// let count = 0;
+// function printCount() {
+//   if (count === 0) {
+//     let count = 3;
+//     console.log("count1:-", count);
+//   }
+//   console.log("count2:-", count);
+// }
+// printCount();
+
+// Question 4:-
+// What is the output of this function:-
+
+console.log(2 + "2");
+console.log(2 - "2");
+console.log("2" - 2);
